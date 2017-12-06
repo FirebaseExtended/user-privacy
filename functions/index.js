e8365f28c1c46a84a1f914e59df704c5e5ff7ada
Rachel Myers <rachelmyers@google.com>
2017-12-06 15:29:00 -0800

    Initial rtdb, storage & firestore scaffold

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+
+//exports.databaseWipeout
+
+// exports.databaseTakeout
+
+// exports.storageWipeout
+
+// exports.storageTakeout
+
+// exports.firestoreWipeout
+
+// exports.firestoreTakeout
